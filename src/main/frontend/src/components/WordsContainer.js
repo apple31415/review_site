c30c344c515ab9a05adf4af9406427601b713617
Ashley Kane <kane.ashleymay@gmail.com>
2020-10-14 15:36:11 -0400

    Components
    I added index pages and show pages for words and languages.  I also fixed an export statement in the word show page.

---
+++ src/main/frontend/src/components/WordsContainer.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+
+const WordsContainer = (props) => {
+  const [words, setWords] = useState([])
+
+  let wordType = props.match.params.words
+
+  useEffect(() => {
+    fetch('api/v1/words')
+    .then(result => result.json())
+    .then(words => {
+      setWords(words)
+    })
+  }, [wordType])
+  
+  let WordsElements = mappedWords.map((word, index) => {
+    return(
+      <div key={index}>
+        <Link to={`/words/${word.id}`}>
+          <h4>Word: {word.name}</h4>
+        </Link>
+        <p>Language: {word.language.name}</p>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      {WordsElements}
+    </div>
+  )
+
+}
+export default WordsContainer 
