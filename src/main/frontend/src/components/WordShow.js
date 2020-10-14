1d8b32a9de1b93814d6c9ea995f159e4da3f7426
Ashley Kane <kane.ashleymay@gmail.com>
2020-10-14 13:01:13 -0400

    Fixes
    No we are not reviewing pets Dan.

--- src/main/frontend/src/components/WordShow.js
+++ src/main/frontend/src/components/WordShow.js
@@ -3,11 +3,11 @@ import ReviewForm from './ReviewForm'
 
 const WordShow = (props) => {
   let wordId = props.match.params.id
-  const [pet, setPet] = useState([])
+  const [word, setWord] = useState([])
   const [newReview, setNewReview] = useState([])
 
   useEffect(() => {
-   fetch('api/v1/${props.location.pathname}')
+   fetch('api/v1${props.location.pathname}')
     .then(result => result.json())
     .then(word => {
       setWord(word)
@@ -23,7 +23,7 @@ const WordShow = (props) => {
     <div>
       <h1>Words Words Words</h1>
       <p>Word: {word.name}</p>
-      <p>Definition: {word.deffinition}</p>
+      <p>Definition: {word.definition}</p>
       <p>Language: {word.name_id}</p>
       {reviewForm}
     </div>
