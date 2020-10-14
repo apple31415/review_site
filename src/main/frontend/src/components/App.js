e934f3db93945616f9842bcb21a9d68002afb63d
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-10-14 13:49:54 -0700

    Implements router switch

--- src/main/frontend/src/components/App.js
+++ src/main/frontend/src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from './Navbar.js';
+import WordsContainer from './WordsContainer.js';
+import WordShow from './WordShow.js';
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Switch>
+          <Route exact path="/words">
+            <WordsContainer />
+          </Route>
+          <Route exact path="/words/:id">
+            <WordShow />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
