94708df5d9b2bfb91363b9907c931f0460861270
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-10-15 10:07:51 -0700

    Fix super aggravating routing bug

--- src/main/java/com/launchacademy/reviews/controllers/UsersAPIController.java
+++ src/main/java/com/launchacademy/reviews/controllers/UsersAPIController.java
@@ -1,10 +1,7 @@
 package com.launchacademy.reviews.controllers;
 
-import com.launchacademy.reviews.models.User;
 import com.launchacademy.reviews.repositories.UserRepository;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
