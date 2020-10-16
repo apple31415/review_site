55d7241934ab97dd8b4c8953832db1bd18499819
MtnGreen <66486684+MtnGreen@users.noreply.github.com>
2020-10-16 11:58:16 -0400

    Update WordForm.js

--- src/main/frontend/src/components/WordForm.js
+++ src/main/frontend/src/components/WordForm.js
@@ -11,34 +11,32 @@ function WordForm() {
   }, []);
   const dropdown = languagesList.map((language) =>
   <option key={language.id} value={language.name}>{language.name}</option>)
+                                     
   return (
-
-<form>
-  <div className="grid-container">
-    <div className="grid-x grid-padding-x">
-      <div className="medium-6 cell">
-        <label>Word
-          <input type="text" placeholder=".medium-6.cell"/>
-        </label>
+    <form>
+      <div className="grid-container">
+        <div className="grid-x grid-padding-x">
+          <div className="medium-6 cell">
+            <label>Word
+              <input type="text" placeholder=".medium-6.cell"/>
+            </label>
+          </div>
+          <div className="medium-6 cell">
+            <label>Definition
+              <input type="text" placeholder=".medium-6.cell"/>
+            </label>
+          </div>
+          <div>
+            <label>Select Language
+              <select>{dropdown}</select>
+            </label>
+          </div>
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </div>
       </div>
-      <div className="medium-6 cell">
-        <label>Definition
-          <input type="text" placeholder=".medium-6.cell"/>
-        </label>
-      </div>
-      <div>
-      <label>Select Language
-        <select>
-          {dropdown}
-        </select>
-      </label>
-      </div>
-      <div>
-       <input type="submit" value="Submit" />
-      </div>
-    </div>
-  </div>
-</form>
+    </form>
   );
 }
 
