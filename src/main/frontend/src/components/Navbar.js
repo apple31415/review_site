415878aced574042ad63cf8dcb7707e1da8bc3dc
MtnGreen <patricemahoney@gmail.com>
2020-10-20 17:13:11 -0400

    not sure what is happening

--- src/main/frontend/src/components/Navbar.js
+++ src/main/frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <div className="top-bar" id="navbar">
       <div className="top-bar-left">
         <ul className="menu" id="menu">
-          <li><Link to="/words"><strong>Word Up</strong></Link></li>
+          <li><Link to="/words"><strong>Word Upp</strong></Link></li>
           <li><Link to="/words/new">Submit a New Word</Link></li>
           <li><Link to="/users/new">Make a User Profile</Link></li>
         </ul>
