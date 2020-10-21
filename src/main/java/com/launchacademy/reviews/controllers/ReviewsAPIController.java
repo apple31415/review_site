d19a27e88a6454b8265f765c4c32f8c783a0d364
MtnGreen <patricemahoney@gmail.com>
2020-10-21 12:05:20 -0400

    deleting together and merging like champs

--- src/main/java/com/launchacademy/reviews/controllers/ReviewsAPIController.java
+++ src/main/java/com/launchacademy/reviews/controllers/ReviewsAPIController.java
@@ -4,8 +4,13 @@ import com.launchacademy.reviews.models.Review;
 import com.launchacademy.reviews.repositories.ReviewRepository;
 import com.launchacademy.reviews.repositories.WordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +29,11 @@ public class ReviewsAPIController {
   @GetMapping
   public Iterable<Review> getAll(){
     return reviewRepository.findAll();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity deleteReview(@PathVariable Integer id){
+    reviewRepository.deleteById(id);
+    return new ResponseEntity(HttpStatus.OK);
   }
 }
