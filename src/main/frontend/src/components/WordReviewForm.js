6e717cbac7c9f687818e315697396c22c14a0a98
MtnGreen <patricemahoney@gmail.com>
2020-10-22 16:51:44 -0400

    more style challenges

--- src/main/frontend/src/components/WordReviewForm.js
+++ src/main/frontend/src/components/WordReviewForm.js
@@ -5,10 +5,10 @@ const WordReviewForm = props => {
   const [users, setUsers] = useState([])
   const [errors, setErrors] = useState({})
   const [reviewForm, setReviewForm] = useState({
-      userId: props.review?.user.id,
-      word: props.word,
-      rating: props.review?.rating,
-      comment: props.review?.comment
+    userId: props.review?.user.id,
+    word: props.word,
+    rating: props.review?.rating,
+    comment: props.review?.comment
   })
 
   useEffect(() => {
@@ -63,20 +63,21 @@ const WordReviewForm = props => {
       fetch(`/api/v1/words/${id}/reviews`, {
         method: props.review ? "PUT" : "POST",
         body: JSON.stringify({
-          ...formData, reviewId : props.review ? props.review.id : -1}),
-        headers: {"Content-Type" : "application/json"}
+          ...formData, reviewId: props.review ? props.review.id : -1
+        }),
+        headers: { "Content-Type": "application/json" }
       })
-      .then(result => {
-        setReviewForm({
-          userId: '',
-          rating: '',
-          comment: ''
-      })
-      })
-      .then(() => {
-      handleClose()
-      })
-    } 
+        .then(result => {
+          setReviewForm({
+            userId: '',
+            rating: '',
+            comment: ''
+          })
+        })
+        .then(() => {
+          handleClose()
+        })
+    }
   }
   let startingUsers = [<option key="-1" value=""></option>]
   let mappedUsers = startingUsers.concat(users.map(user => {
@@ -84,40 +85,39 @@ const WordReviewForm = props => {
   }))
 
   return (
-    <>
+    <div className="row">
       <button onClick={handleClose}>Close</button>
-      <form onSubmit={handleSubmit}>
-        <label>User
+      <form className="col s4 offset-s4" onSubmit={handleSubmit}>
+        <div className="row">
+          <label>User
           <p className="error">{errors.userId}</p>
-          <select onChange={handleChange} name="userId" id="userId" value={reviewForm.userId}>
-            {mappedUsers}
-          </select>
-        </label>
-        <div>
-          <p>Rating:</p>
-          <p className="error">{errors.rating}</p>
-          <label>One
-            <input onChange={handleChange} type="radio" id="rating" name="rating" value="1" checked={reviewForm.rating == 1} />
-          </label>
-          <label>Two
-            <input onChange={handleChange} type="radio" id="rating" name="rating" value="2" checked={reviewForm.rating == 2}/>
-          </label>
-          <label>Three
-            <input onChange={handleChange} type="radio" id="rating" name="rating" value="3" checked={reviewForm.rating == 3}/>
-          </label>
-          <label>Four
-            <input onChange={handleChange} type="radio" id="rating" name="rating" value="4" checked={reviewForm.rating == 4}/>
-          </label>
-          <label>Five
-            <input onChange={handleChange} type="radio" id="rating" name="rating" value="5" checked={reviewForm.rating == 5}/>
+            <select onChange={handleChange} name="userId" id="userId" value={reviewForm.userId}>
+              {mappedUsers}
+            </select>
           </label>
         </div>
-        <label>Review
+        <div className="row">
+          <p>Rating:</p>
+          <p className="error">{errors.rating}</p>
+          <input className="with-gap" onChange={handleChange} type="radio" id="rating1" name="rating" value="1" checked={reviewForm.rating == 1} />
+          <label htmlFor="rating1"><span>One</span></label>
+          <input onChange={handleChange} type="radio" id="rating2" name="rating" value="2" checked={reviewForm.rating == 2} />
+          <label htmlFor="rating2"><span>Two</span></label>
+          <input onChange={handleChange} type="radio" id="rating3" name="rating" value="3" checked={reviewForm.rating == 3} />
+          <label htmlFor="rating3"><span>Three</span></label>
+          <input onChange={handleChange} type="radio" id="rating4" name="rating" value="4" checked={reviewForm.rating == 4} />
+          <label htmlFor="rating4"><span>Four</span></label>
+          <input onChange={handleChange} type="radio" id="rating5" name="rating" value="5" checked={reviewForm.rating == 5} />
+          <label htmlFor="rating5"><span>Five</span></label>
+        </div>
+        <div className="row">
+          <label>Review
           <textarea onChange={handleChange} name="comment" id="comment" value={reviewForm.comment} />
-        </label>
+          </label>
+        </div>
         <input type="submit" value="submit" />
       </form>
-    </>
+    </div>
   )
 }
 
